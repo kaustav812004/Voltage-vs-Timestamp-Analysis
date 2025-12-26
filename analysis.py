d0a85b6ff1d8ea5d8e69d73f7c020f3818d80373
Kaustav Ghosh <kaustav812004@gmail.com>
2025-12-26 15:58:13 +0530

    Initial Submission of voltage analysis

---
+++ analysis.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
+
+
+def load_data():
+    df = pd.read_csv('data/Sample_Data.csv')
+    df["Timestamp"] = pd.to_datetime(df["Timestamp"], dayfirst=True)
+    return df
+
+def MA(df):
+    df["MA_1000"] = df["Values"].rolling(1000).mean()
+    df["MA_5000"] = df["Values"].rolling(5000).mean()
+    return df
+
+def plot_voltage(df, output_path="outputs/plots"):
+    plt.figure(figsize=(12, 6))
+    plt.plot(df["Timestamp"], df["Values"], color='blue')
+    plt.plot(df["Timestamp"], df["MA_1000"], color='red')
+    plt.plot(df["Timestamp"], df["MA_5000"], color='green')
+    plt.xlabel("Timestamp")
+    plt.ylabel("Voltage")
+    plt.title("Values with 1000 and 5000 Value Moving Averages")
+    plt.legend(["Values", "MA 1000", "MA 5000"])
+    plt.tight_layout()
+    plt.savefig(f"{output_path}/value_ma_plot.png")
+    plt.show()
+    
+    
+def peak(df):
+    peak, _ = find_peaks(df['Values'])
+    peak_df = df.iloc[peak][['Timestamp', 'Values']]
+    return peak_df
+
+def low(df):
+    low, _ = find_peaks(-df['Values'])
+    low_df = df.iloc[low][['Timestamp', 'Values']]
+    return low_df
+
+def below_k(df, k, path):
+    below_df = df.loc[df['Values'] < k, ['Timestamp', 'Values']]
+    return below_df
+
+def downslopeAccelerate(df):
+    slope = df['Values'].diff()
+    slope_change = slope.diff()
+    down_acc = df[(slope < 0) & (slope_change < 0)][['Timestamp']]
+    return down_acc['Timestamp']
+
+def main():
+    df = load_data()
+    df = MA(df)
+    plot_voltage(df, output_path="outputs/plots")
+    peak_df = peak(df)
+    low_df = low(df)
+    peak_df.to_csv('outputs/tables/peaks.csv', index=False)
+    low_df.to_csv('outputs/tables/lows.csv', index=False)
+    below_20_df = below_k(df, 20, 'outputs/tables/below_20.csv')
+    down_timestamp = downslopeAccelerate(df)
+    print("Downward Accelerating Timestamps:")
+    print(down_timestamp)
---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+from analysis import load_data, MA, plot_voltage, peak, low, below_k, downslopeAccelerate
+
+
+st.set_page_config(page_title="Voltage Analysis", layout="wide")
+
+st.title("Voltage Time-Series Analysis")
+st.write("Greencel Assignment")
+
+df = load_data()
+df = MA(df)
+
+st.subheader("Voltage with Moving Averages")
+
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.plot(df["Timestamp"], df["Values"], label = "Values", color='blue')
+ax.plot(df["Timestamp"], df["MA_1000"], label = "MA 1000", color='red')
+ax.plot(df["Timestamp"], df["MA_5000"], label = "MA 5000", color='green')
+ax.set_xlabel("Timestamp")
+ax.set_ylabel("Voltage")
+ax.set_title("Values with 1000 and 5000 Value Moving Averages")
+ax.legend()
+st.pyplot(fig)
+
+peak_df = peak(df)
+low_df = low(df)
+
+col1, col2 = st.columns(2)
+with col1:
+    st.subheader("Peaks in Voltage Data")
+    st.dataframe(peak_df)
+with col2:
+    st.subheader("Lows in Voltage Data")
+    st.dataframe(low_df)
+
+st.subheader("Values Below Threshold")
+threshold = st.number_input("Set Threshold Value:", value=20)
+below_df = below_k(df, threshold, None)
+st.dataframe(below_df)
+
+st.subheader("Timestamps with Downward Accelerating Values")
+down_timestamp = downslopeAccelerate(df)
+st.write("Timestamps wehere downward acceleration occurs:")
+st.dataframe(down_timestamp)
